00f37dd9f60c46d53dd72e4b7df7a03a1432cf7b
hamzashabbeer <hamzashabbeer@gmail.com>
2025-01-06 00:23:23 +0500

    chore: update Next.js config with build settings and image configurations

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    typedRoutes: true,
+  },
   images: {
     remotePatterns: [
       {
